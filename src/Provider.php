d853d63dd36d86ff6cb7427f5f766e0ff732f7a5
atan <tanshiqi@gmail.com>
2019-01-23 17:12:06 +0800

    wip

--- src/Provider.php
+++ src/Provider.php
@@ -89,7 +89,6 @@ class Provider extends AbstractProvider implements ProviderInterface
 
     protected function mapUserToObject(array $user)
     {
-        logger($user);
         return (new User())->setRaw($user)->map([
             'id'       => $user['userid'],
             'name'     => $user['name'],
